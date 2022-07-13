e98d5a6d8dc62c6e83798f76f14de1e00331da06
Golder12 <golderalikaya@gmail.com>
2022-07-13 17:20:15 +0300

    interfaces for the customer and meter

---
+++ backend/src/main/java/org/swamp/backend/core/services/MeterService.java
@@ -0,0 +1,12 @@
+package org.swamp.backend.core.services;
+
+import org.swamp.backend.models.meter.Meter;
+
+/**
+ * Responsible for all CRUD operations on {@link Meter}
+ * @author Doki
+ *
+ */
+public interface MeterService extends GenericService<Meter> {
+
+}
